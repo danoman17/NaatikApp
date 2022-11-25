a3605b5e81688ae2b26b6ee625bd5ff7cbb9000f
Alejandro López <alexlohe25@gmail.com>
2022-11-25 12:42:25 -0600

    Text slider rendering hotfix

--- naatik-app/src/components/reusable/TextSlider/TextSlider.js
+++ naatik-app/src/components/reusable/TextSlider/TextSlider.js
@@ -1,102 +1,163 @@
-import React, { useEffect, useState } from 'react'
-import {ShowAmount} from '../../../routeIndex';
-import './TextSlider.css'
-import { Button, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { ShowAmount } from "../../../routeIndex";
+import "./TextSlider.css";
+import {
+  Button,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
-const TextSlider = ({info, infoLateralBar}) => {
- 
+const TextSlider = ({ info, infoLateralBar }) => {
   const navigate = useNavigate();
   const data = [];
 
-  
   const goToPerfil = () => {
-    navigate('/PantallaPerfil',
-    {
+    navigate("/PantallaPerfil", {
       state: {
         clusterInfo: infoLateralBar[0],
         churns: infoLateralBar[1],
-      }
+      },
     });
   };
-
-
-  
 
   // creamos el metodo que calcula los porcentajes de la PieChart.
 
   const calculatePercentages = () => {
-    info.map((cluster)=> {
+    info.map((cluster) => {
       data.push(cluster["bill amount"]);
     });
   };
 
   calculatePercentages();
 
-
-
-
   return (
+    <Tabs
+      variant="unstyled"
+      boxShadow="2xl"
+      p="3"
+      rounded="md"
+      bg="white"
+      w="80%"
+      h="80%"
+      overflow="scroll"
+    >
+      <TabList
+        h="15%"
+        w="100%"
+        display="flex"
+        justifyContent="space-around"
+        alignItems="center"
+        padding="10px"
+      >
+        <Tab
+          _selected={{ color: "white", bg: "#E34956", borderRadius: "50px" }}
+          className="btn-textslider-1"
+          aria-selected="true"
+        >
+          {" "}
+          <p className="txt-btn-textSlider">Normal</p>{" "}
+        </Tab>
+        <Tab
+          _selected={{ color: "white", bg: "#70AE47", borderRadius: "50px" }}
+          className="btn-textslider-2"
+          aria-selected="true"
+        >
+          {" "}
+          <p className="txt-btn-textSlider">Bajo</p>{" "}
+        </Tab>
+        <Tab
+          _selected={{ color: "white", bg: "#EE7D30", borderRadius: "50px" }}
+          className="btn-textslider-3"
+        >
+          <p className="txt-btn-textSlider">Medio</p>
+        </Tab>
+        <Tab
+          _selected={{ color: "white", bg: "#982B52", borderRadius: "50px" }}
+          className="btn-textslider-4"
+        >
+          <p className="txt-btn-textSlider">Alto</p>
+        </Tab>
+      </TabList>
 
+      <TabPanels h="85%" w="100%">
+        <TabPanel h="100%" w="100%">
+          <div className="contenedor-general-resumen">
+            <div className="parte-arriba">
+              {
+                <ShowAmount
+                  Label={"Total de Factura"}
+                  Amount={data[0].toFixed(2)}
+                ></ShowAmount>
+              }
+            </div>
+            <div className="parte-abajo">
+              <Button colorScheme="green" onClick={goToPerfil}>
+                Ver más
+              </Button>
+            </div>
+          </div>
+        </TabPanel>
 
-      <Tabs variant='unstyled' boxShadow='2xl' p='3' rounded='md' bg='white' w='80%' h='80%' overflow='scroll'>
+        <TabPanel h="100%" w="100%">
+          <div className="contenedor-general-resumen">
+            <div className="parte-arriba">
+              {
+                <ShowAmount
+                  Label={"Total de Factura"}
+                  Amount={data[1].toFixed(2)}
+                ></ShowAmount>
+              }
+            </div>
+            <div className="parte-abajo">
+              <Button colorScheme="green" onClick={goToPerfil}>
+                Ver más
+              </Button>
+            </div>
+          </div>
+        </TabPanel>
 
-        <TabList h='15%' w='100%' display='flex' justifyContent='space-around' alignItems='center' padding='10px'>
-          <Tab _selected={{ color: 'white', bg: '#E34956', borderRadius:'50px' }} className='btn-textslider-1' aria-selected='true'> <p className='txt-btn-textSlider'>Normal</p> </Tab>
-          <Tab _selected={{ color: 'white', bg: '#70AE47', borderRadius:'50px' }} className='btn-textslider-2' aria-selected='true'> <p className='txt-btn-textSlider'>Bajo</p> </Tab>
-          <Tab _selected={{ color: 'white', bg: '#EE7D30', borderRadius:'50px' }} className='btn-textslider-3'><p className='txt-btn-textSlider'>Medio</p></Tab>
-          <Tab _selected={{ color: 'white', bg: '#982B52', borderRadius:'50px' }} className='btn-textslider-4'><p className='txt-btn-textSlider'>Alto</p></Tab>
-        </TabList>
+        <TabPanel h="100%" w="100%">
+          <div className="contenedor-general-resumen">
+            <div className="parte-arriba">
+              {
+                <ShowAmount
+                  Label={"Total de Factura"}
+                  Amount={data[2].toFixed(2)}
+                ></ShowAmount>
+              }
+            </div>
+            <div className="parte-abajo">
+              <Button colorScheme="green" onClick={goToPerfil}>
+                Ver más
+              </Button>
+            </div>
+          </div>
+        </TabPanel>
 
-            <TabPanels h='85%' w='100%'>
-                  <TabPanel h='100%' w='100%'>
-                    <div className='contenedor-general-resumen'>
-                      <div className='parte-arriba'>
-                        {/* <ShowAmount Label={'Total de Factura'} Amount={data[0].toFixed(2)}></ShowAmount> */}
-                      </div>
-                      <div className='parte-abajo'>
-                        <Button colorScheme='green' onClick={goToPerfil} >Ver más</Button>
-                      </div>
-                    </div>
-                    
-                  </TabPanel>
+        <TabPanel h="100%" w="100%">
+          <div className="contenedor-general-resumen">
+            <div className="parte-arriba">
+              {
+                <ShowAmount
+                  Label={"Total de Factura"}
+                  Amount={data[3].toFixed(2)}
+                ></ShowAmount>
+              }
+            </div>
+            <div className="parte-abajo">
+              <Button colorScheme="green" onClick={goToPerfil}>
+                Ver más
+              </Button>
+            </div>
+          </div>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
+  );
+};
 
-                  <TabPanel h='100%' w='100%'>
-                    <div className='contenedor-general-resumen'>
-                      <div className='parte-arriba'>
-                        {/* <ShowAmount Label={'Total de Factura'} Amount={data[1].toFixed(2)}></ShowAmount> */}
-                      </div>
-                      <div className='parte-abajo'>
-                        <Button colorScheme='green' onClick={goToPerfil} >Ver más</Button>
-                      </div>
-                    </div>
-                  </TabPanel>
-
-                  <TabPanel h='100%' w='100%'>
-                    <div className='contenedor-general-resumen'>
-                      <div className='parte-arriba'>
-                        {/* <ShowAmount Label={'Total de Factura'} Amount={data[2].toFixed(2)}></ShowAmount> */}
-                      </div>
-                      <div className='parte-abajo'>
-                        <Button colorScheme='green' onClick={goToPerfil}>Ver más</Button>
-                      </div>
-                    </div>
-                  </TabPanel>
-
-                  <TabPanel h='100%' w='100%'>
-                    <div className='contenedor-general-resumen'>
-                      <div className='parte-arriba'>
-                        {/* <ShowAmount Label={'Total de Factura'} Amount={data[3].toFixed(2)}></ShowAmount> */}
-                      </div>
-                      <div className='parte-abajo'>
-                        <Button colorScheme='green' onClick={goToPerfil}>Ver más</Button>
-                      </div>
-                    </div>
-                  </TabPanel>
-                  
-            </TabPanels>
-      </Tabs>
-  )
-}
-
-export default TextSlider
+export default TextSlider;
--- naatik-app/src/views/Cliente/PantallaDividida/pantallaDividida.js
+++ naatik-app/src/views/Cliente/PantallaDividida/pantallaDividida.js
@@ -1,10 +1,17 @@
-import React, { useEffect, useMemo, useState } from 'react'
-import { useLocation } from 'react-router-dom';
-import { Navbar, TarjetaMaestra, BarraLateral, PieChart, CirculoPorcentaje, TextSlider, BarraNombreArchivo } from '../../../routeIndex';
-import './pantallaDividida.css'
+import React, { useEffect, useMemo, useState } from "react";
+import { useLocation } from "react-router-dom";
+import {
+  Navbar,
+  TarjetaMaestra,
+  BarraLateral,
+  PieChart,
+  CirculoPorcentaje,
+  TextSlider,
+  BarraNombreArchivo,
+} from "../../../routeIndex";
+import "./pantallaDividida.css";
 
 const PantallaDividida = () => {
-
   const variableParaGuardarDatos = [];
   // variable donde guardaremos los valores de churn
   const [churnvalues, setChurnvalues] = useState([]);
@@ -20,44 +27,37 @@ const PantallaDividida = () => {
 
   // usamos el useEffect para poder ejecutar la consulta una vez accedido a esta view
 
-    const fetchSliderValues = async () => {
-      try {
-        const response = await fetch('http://localhost:8080/get_churn_segment/');
-        const json = await response.json();
-        setChurnvalues(await json.churn_segment);
-      } catch (error) {
-        console.log("error", error);
-      }
+  const fetchSliderValues = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/get_churn_segment/");
+      const json = await response.json();
+      setChurnvalues(await json.churn_segment);
+    } catch (error) {
+      console.log("error", error);
     }
+  };
 
-    
+  // creamos metodo para mandar a llamar al endpoint que nos traerá la informacion del cluster, dependiendo del cluster actual
+  const fetchClusterInfo = async (clusterNumer) => {
+    try {
+      console.log("fetching cluster info");
+      const response = await fetch(
+        "http://localhost:8080/cluster/Cluster_" + clusterNumer
+      );
+      const json = await response.json();
+      console.log("perfil aaaaa", json.perfil);
+      setDataCluster(await json.perfil);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
 
-
-
-    // creamos metodo para mandar a llamar al endpoint que nos traerá la informacion del cluster, dependiendo del cluster actual
-    const fetchClusterInfo = async (clusterNumer) => {
-      console.log(clusterNumer);
-      try {
-        const response = await fetch('http://localhost:8080/cluster/Cluster_' + clusterNumer);
-        const json = await response.json();
-        setDataCluster(await json.perfil);
-      } catch (error) {
-        console.log("error", error);
-      }
-    };
-
-    useEffect(()=> {
-      // traemos los valores del churn
-      fetchSliderValues();
-      fetchClusterInfo(currentCluster);
-      console.log(dataCluster);
-
-    },[]);
-
-
-
-
-
+  useEffect(() => {
+    // traemos los valores del churn
+    fetchSliderValues();
+    fetchClusterInfo(currentCluster);
+    console.log("useEffect aaaaaaaa", dataCluster);
+  }, []);
 
   return (
     <>
@@ -69,10 +69,8 @@ const PantallaDividida = () => {
 
       {/* general container for the spacework */}
       <TarjetaMaestra>
-
         {/* container for comodation purposes */}
-        <div className='contenedor-general-panel'>
-
+        <div className="contenedor-general-panel">
           {/* lateral navbar section */}
           <BarraLateral
             Bajo={churnvalues[0]}
@@ -80,79 +78,66 @@ const PantallaDividida = () => {
             Alto={churnvalues[2]}
             arrayInfo={state.clustersInfo}
             clickBtn={setCurrentCluster}
-          >
-          </BarraLateral>
+          ></BarraLateral>
 
           {/* section for general content */}
-          <div className='contendor-general-tarjeta'>
-            <div className='tarjeta-maestra'>
+          <div className="contendor-general-tarjeta">
+            <div className="tarjeta-maestra">
               {/* we can add components here.... */}
 
               {/* title/instructions */}
-              <div className='contenedor-der-pantalladiv'>
-                <div className='contendeor-PieChart'>
-                  <div className='contenedor-PieChart-header'>
+              <div className="contenedor-der-pantalladiv">
+                <div className="contendeor-PieChart">
+                  <div className="contenedor-PieChart-header">
                     <p>Distribución de grupos</p>
                   </div>
-                  <div className='contenedor-PieChart-chart'>
-                    {
-                      (currentCluster !== -1) ?
-                        (
-                          <PieChart number={currentCluster} infoCluster={dataCluster}>
-
-                          </PieChart>
-                        ) :
-                        (
-                          <></>
-                        )
-                    }
-
+                  <div className="contenedor-PieChart-chart">
+                    {currentCluster !== -1 ? (
+                      <PieChart
+                        number={currentCluster}
+                        infoCluster={dataCluster}
+                      ></PieChart>
+                    ) : (
+                      <></>
+                    )}
                   </div>
                 </div>
-                <div className='contenedor-porcentaje'>
-
-                  <div className='contenedor-porcentaje-header'>
-                    <p className='texto-porcentaje'> Porcentaje del dataset usado </p>
+                <div className="contenedor-porcentaje">
+                  <div className="contenedor-porcentaje-header">
+                    <p className="texto-porcentaje">
+                      {" "}
+                      Porcentaje del dataset usado{" "}
+                    </p>
                   </div>
-                  <div className='contenedor-porcentaje-graph'>
-                    {
-                      (currentCluster !== -1) ?
-                        (
-                          <CirculoPorcentaje porcentaje={state.clustersInfo[currentCluster].percentage}></CirculoPorcentaje>
-                        ) :
-                        (
-                          <></>
-                        )
-                    }
-
+                  <div className="contenedor-porcentaje-graph">
+                    {currentCluster !== -1 ? (
+                      <CirculoPorcentaje
+                        porcentaje={
+                          state.clustersInfo[currentCluster].percentage
+                        }
+                      ></CirculoPorcentaje>
+                    ) : (
+                      <></>
+                    )}
                   </div>
-
                 </div>
-
               </div>
-              <div className='contenedor-izq-pantalladiv'>
-
-                {
-                  (currentCluster !== -1) ?
-                    (
-                      <TextSlider
-                        info={dataCluster}
-                        infoLateralBar={[state.clustersInfo, churnvalues]}
-                      >
-                      </TextSlider>
-                    ) : (<></>)
-                }
-
+              <div className="contenedor-izq-pantalladiv">
+                {currentCluster !== -1 && dataCluster.length > 0 ? (
+                  <TextSlider
+                    info={dataCluster}
+                    infoLateralBar={[state.clustersInfo, churnvalues]}
+                  ></TextSlider>
+                ) : (
+                  <></>
+                )}
               </div>
-
             </div>
           </div>
-
         </div>
-
       </TarjetaMaestra>
     </>
-  )
-}
+  );
+};
 
 export default PantallaDividida;

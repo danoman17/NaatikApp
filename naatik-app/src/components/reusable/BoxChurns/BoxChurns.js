e8b48e7d7a54976291ad7567e55d497cc088e06c
Daniel Flores Rodríguez <64983319+danoman17@users.noreply.github.com>
2022-11-30 16:00:19 -0600

    adjust boxChurn component

--- naatik-app/src/components/reusable/BoxChurns/BoxChurns.js
+++ naatik-app/src/components/reusable/BoxChurns/BoxChurns.js
@@ -2,25 +2,65 @@ import React from 'react'
 import { AiOutlineFullscreenExit } from "react-icons/ai";
 import './BoxChurns.css'
 
-const BoxChurns = ({BajoEnd, MedEnd, AltoEnd}) => {
-  return (
-    <>
-        <div className='contenedor-general-box'>
-            <button className='btn-header-box'>
-                <p>Porcentajes Churn</p>
-                <AiOutlineFullscreenExit/>
-            </button>
+const BoxChurns = ({ BajoEnd, MedEnd, AltoEnd }) => {
 
-            <div className='contenedor-porcentajes'>
-                <p className="porcentaje-normal"> 0% - {BajoEnd}%</p>
-                <p className="porcentaje-baja"> {BajoEnd} - {MedEnd}%</p>
-                <p className="porcentaje-media"> {MedEnd}% - {AltoEnd}%</p>
-                <p className="porcentaje-alta"> {AltoEnd}% - 100%</p>
 
+    const handleClickDummie = (e) => {
+        e.preventDefault();
+    }
+    
+    return (
+        <>
+            <div className='contenedor-general-box'>
+                <button className='btn-header-box' onClick={handleClickDummie}>
+                    <p>Porcentajes Churn</p>
+                    <AiOutlineFullscreenExit size={'20px'}/>
+                </button>
+
+                <div className='contenedor-porcentajes'>
+                    <div className='contenedor-porcentaje'>
+                        <div className='contenedor-porcentaje-izq'>
+                            <p className='incial-contenedor-porcentaje-permanente'> P: </p>
+                        </div>
+                        <div className='contenedor-porcentaje-der'>
+                            
+                            <p className="porcentaje-normal">0% - {BajoEnd}%</p>
+                        </div>
+                    </div>
+
+                    <div className='contenedor-porcentaje'>
+                        <div className='contenedor-porcentaje-izq'>
+                            <p className='incial-contenedor-porcentaje-bajo'> B: </p>
+                        </div>
+                        <div className='contenedor-porcentaje-der'>
+                            
+                            <p className="porcentaje-baja"> {BajoEnd} - {MedEnd}%</p>
+
+                        </div>
+                    </div>
+
+                    <div className='contenedor-porcentaje'>
+                        <div className='contenedor-porcentaje-izq'>
+                            <p className='incial-contenedor-porcentaje-medio'> M: </p>
+                        </div>
+                        <div className='contenedor-porcentaje-der'>
+                            <p className="porcentaje-media"> {MedEnd}% - {AltoEnd}%</p>
+                        </div>
+                    </div>
+
+                    <div className='contenedor-porcentaje'>
+                        <div className='contenedor-porcentaje-izq'>
+                            <p className='incial-contenedor-porcentaje-alto'> A: </p>
+                        </div>
+                        <div className='contenedor-porcentaje-der'>
+                            
+                            <p className="porcentaje-alta"> {AltoEnd}% - 100%</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default BoxChurns;
